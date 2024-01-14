95d2b756f978d666d395ab2634ca8594b5f8fb05
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-01-15 01:56:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Spline from '@splinetool/react-spline';
+
+function App() {
+  
+
+  return (
+    <main >
+      <Spline style={{width:'100vw', height:'100vh'}} scene="https://prod.spline.design/BTCAasgmlogIIShr/scene.splinecode" />
+    </main>
+  )
+}
+
+export default App
